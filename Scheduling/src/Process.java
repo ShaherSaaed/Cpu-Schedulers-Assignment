066e2eaf6c78dca2e80260f4a7721f389496f41e
ShaherSaaed <shahersa3ed004@gmail.com>
2024-11-29 18:39:14 +0200

    Worked on process class

--- Scheduling/src/Process.java
+++ Scheduling/src/Process.java
@@ -1,3 +1,94 @@
 public class Process {
+    private final int id;
+    private final int priority;
+    private final int arrivalTime;
+    private final int burstTime;
+    private int remainingBurstTime;
+    private int startTime;
+    private int completionTime;
+    private int turnaroundTime;
+    private int waitingTime;
+    private int quantum;
+
+    public Process(int id, int priority, int arrivalTime, int burstTime){
+        this.id = id;
+        this.priority = priority;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.remainingBurstTime = burstTime;
+        this.startTime = -1;
+        this.completionTime = 0;
+        this.turnaroundTime = 0;
+        this.waitingTime = 0;
+        this.quantum = 0;
+    }
+
+    /////////////////////////////Setters/////////////////////////////
+
+    public void setRemainingBurstTime(int remainingBurstTime){
+        this.remainingBurstTime = remainingBurstTime;
+    }
+
+    public void setStartTime(int startTime){
+        this.startTime = startTime;
+    }
+
+    public void setCompletionTime(int completionTime){
+        this.completionTime = completionTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime){
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    public void setQuantum(int quantum){
+        this.quantum = quantum;
+    }
+
+    public void setWaitingTime(int waitingTime){
+        this.waitingTime = waitingTime;
+    }
+
+    /////////////////////////////Getters/////////////////////////////
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getRemainingBurstTime(){
+        return remainingBurstTime;
+    }
+
+    public int getStartTime(){
+        return startTime;
+    }
+
+    public int getCompletionTime(){
+        return completionTime;
+    }
+
+    public int getTurnaroundTime(){
+        return turnaroundTime;
+    }
+
+    public int getQuantum(){
+        return quantum;
+    }
+
+    public int getWaitingTime(){
+        return waitingTime;
+    }
 
 }
