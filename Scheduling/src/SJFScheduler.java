1c708c6f00ee57364dcf67221eb679750c87778d
ShaherSaaed <shahersa3ed004@gmail.com>
2024-11-30 18:46:53 +0200

    Working on it still

--- Scheduling/src/SJFScheduler.java
+++ Scheduling/src/SJFScheduler.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class SJFScheduler implements Scheduler {
+    ArrayList<Process> processes;
+
+    public SJFScheduler(ArrayList<Process> processes) {
+        this.processes = processes;
+    }
+
+    @Override
+    public void execute() {
+        processes.sort(Comparator.comparingInt(Process::getBurstTime)
+                .thenComparingInt(Process::getArrivalTime));
+
+        int currentTime = 0;
+        int maxWaitingTime = 10;
+        int completedProcess = 0;
+
+        while (completedProcess < processes.size()) {
+            Process selectedProcess = null;
+
+            for (Process process : processes) {
+                {
+                    if (process != null && process.getArrivalTime() <= currentTime) {
+                        if (selectedProcess == null || process.getBurstTime() < selectedProcess.getBurstTime()) {
+                            selectedProcess = process;
+                        }
+                    }
+                }
+
+                if (selectedProcess == null) {
+                    currentTime++;
+                    continue;
+                }
+
+                if (currentTime - selectedProcess.getArrivalTime() > maxWaitingTime) {
+                    selectedProcess.decreaseBurstTime();
+                }
+
+                assert process != null;
+
+                process.setStartTime(currentTime);
+
+                currentTime += process.getBurstTime();
+
+                selectedProcess.setCompletionTime(currentTime);
+
+                selectedProcess.setTurnaroundTime(selectedProcess.getCompletionTime() - selectedProcess.getArrivalTime());
+
+                selectedProcess.setWaitingTime(selectedProcess.getTurnaroundTime() - selectedProcess.getBurstTime());
+
+                completedProcess++;
+
+            }
+        }
+    }
+
 }
